c470b20ba0654195acf5b64329189864b1d7346b
Alexander Leonchik <webinfo.alexander+github@gmail.com>
2024-06-13 07:46:20 +0300

    refactoring

--- error.go
+++ error.go
@@ -1,5 +1,10 @@
 package werr
 
+import (
+	"errors"
+)
+
+// wrapError represents an error with additional context such as caller, function name, and message.
 type wrapError struct {
 	caller   string
 	err      error
@@ -7,6 +12,7 @@ type wrapError struct {
 	msg      string
 }
 
+// newError creates a new wrapped error with caller information and additional message.
 func newError(err error, msg string) error {
 	sourceCaller, funcName := caller(defaultCallerSkip)
 
@@ -18,23 +24,16 @@ func newError(err error, msg string) error {
 	}
 }
 
-// Unwrap returns the result of calling the Unwrap method on err,
-// if err's type contains an Unwrap method returning error.
-// Otherwise, Unwrap returns nil.
+// Unwrap calls errors.Unwrap to retrieve the underlying error.
 func Unwrap(err error) error {
-	u, ok := err.(interface{ Unwrap() error })
-	if !ok {
-		return nil
-	}
-
-	return u.Unwrap()
+	return errors.Unwrap(err)
 }
 
 // Cause recursively traverses the wrapped errors and returns the innermost non-wrapped error.
 // It checks for both Cause() and Unwrap() methods.
 func Cause(err error) error {
 	for {
-		// Check if the error implements the causer interface
+		// Check if the error implements the Cause() method
 		if e, ok := err.(interface{ Cause() error }); ok {
 			err = e.Cause()
 
@@ -53,30 +52,42 @@ func Cause(err error) error {
 	}
 }
 
+// Error returns a string representation of the wrapped error.
 func (e wrapError) Error() string {
 	return errorStackMarshaler(e.caller, e.err, e.funcName, e.msg)
 }
 
+// ErrorFormat returns a custom formatted string representation of the wrapped error.
 func (e wrapError) ErrorFormat(fn ErrorStackMarshalerFn) string {
 	return fn(e.caller, e.err, e.funcName, e.msg)
 }
 
+// Unwrap returns the underlying error wrapped by this structure.
 func (e wrapError) Unwrap() error {
 	return e.err
 }
 
+// Cause returns the cause of the wrapped error by recursively checking for Cause() and Unwrap() methods.
 func (e wrapError) Cause() error {
+	var wErr wrapError
+	for errors.As(e.err, &wErr) {
+		e.err = wErr.Unwrap()
+	}
+
 	return e.err
 }
 
+// Caller returns the caller information associated with the wrapped error.
 func (e wrapError) Caller() string {
 	return e.caller
 }
 
+// FuncName returns the function name associated with the wrapped error.
 func (e wrapError) FuncName() string {
 	return e.funcName
 }
 
+// Msg returns the additional message associated with the wrapped error.
 func (e wrapError) Msg() string {
 	return e.msg
 }
